83e14c948076f6de3d20dac8f328b1cad1f57125
Selim Beltrán <jarbeltr@espol.edu.ec>
2025-05-20 13:31:43 -0500

    Nuevo_Archivo

---
+++ file.py
@@ -0,0 +1,20 @@
+# Lista de las 10 mejores canciones
+canciones = [
+    "Bohemian Rhapsody - Queen",
+    "Imagine - John Lennon",
+    "Stairway to Heaven - Led Zeppelin",
+    "Hey Jude - The Beatles",
+    "Smells Like Teen Spirit - Nirvana",
+    "Hotel California - Eagles",
+    "Like a Rolling Stone - Bob Dylan",
+    "Billie Jean - Michael Jackson",
+    "Sweet Child O' Mine - Guns N' Roses",
+    "Wonderwall - Oasis"
+]
+
+# Ordenar la lista en orden ascendente (alfabético)
+canciones_ordenadas = sorted(canciones)
+
+# Imprimir la lista ordenada
+for cancion in canciones_ordenadas:
+    print(cancion)
